a395fef53277be69b41f52ccc82268ab0c92cb2a
Alfrdzley <2311102441242@umkt.ac.id>
2024-06-20 12:16:28 +0800

    chore: create new API key

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ async function fetchWeather() {
   const weatherDataSection = document.getElementById("weather-data");
   weatherDataSection.style.display = "block";
 
-  const apiKey = "a15d468ad08efdb31f19a01c818320c3";
+  const apiKey = "0a7a46fba078a4646ffc8c38f0a3c67c";
 
   if (searchInput == "") {
     weatherDataSection.innerHTML = `
-      <div>
-        <h2>Empty Input!</h2>
-        <p>Please try again with a valid <u>city name</u>.</p>
-      </div>
-      `;
+    <div>
+      <h2>Empty Input!</h2>
+      <p>Please try again with a valid <u>city name</u>.</p>
+    </div>
+    `;
     return;
   }
 
@@ -32,11 +32,11 @@ async function fetchWeather() {
     if (data.length == 0) {
       console.log("Something went wrong here.");
       weatherDataSection.innerHTML = `
-        <div>
-          <h2>Invalid Input: "${searchInput}"</h2>
-          <p>Please try again with a valid <u>city name</u>.</p>
-        </div>
-        `;
+      <div>
+        <h2>Invalid Input: "${searchInput}"</h2>
+        <p>Please try again with a valid <u>city name</u>.</p>
+      </div>
+      `;
       return;
     } else {
       return data[0];
@@ -47,21 +47,20 @@ async function fetchWeather() {
     const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
     const response = await fetch(weatherURL);
 
-    // Step d. Display the weather data
     const data = await response.json();
     weatherDataSection.style.display = "flex";
     weatherDataSection.innerHTML = `
-        <img src="https://openweathermap.org/img/wn/${
-          data.weather[0].icon
-        }.png" alt="${data.weather[0].description}" width="100" />
-        <div>
-          <h2>${data.name}</h2>
-          <p><strong>Temperature:</strong> ${Math.round(
-            data.main.temp - 273.15
-          )}°C</p>
-          <p><strong>Description:</strong> ${data.weather[0].description}</p>
-        </div>
-      `;
+      <img src="https://openweathermap.org/img/wn/${
+        data.weather[0].icon
+      }.png" alt="${data.weather[0].description}" width="100" />
+      <div>
+        <h2>${data.name}</h2>
+        <p><strong>Temperature:</strong> ${Math.round(
+          data.main.temp - 273.15
+        )}°C</p>
+        <p><strong>Description:</strong> ${data.weather[0].description}</p>
+      </div>
+    `;
   }
 
   document.getElementById("search").value = "";
